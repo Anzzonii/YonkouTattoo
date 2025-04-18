1d8ef55dd23da3459cbc3529494aab33e3719166
Anzzonii <antonioserramitjana2005@gmail.com>
2025-04-18 20:47:17 +0200

    Creación del repositorio

---
+++ src/main/java/com/AntonioP/YonkouTattoo/YonkouTattooApplication.java
@@ -0,0 +1,13 @@
+package com.AntonioP.YonkouTattoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YonkouTattooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YonkouTattooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AntonioP/YonkouTattoo/YonkouTattooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AntonioP.YonkouTattoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YonkouTattooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
