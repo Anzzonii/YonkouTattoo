3e78bba9fe6d5a7f6c62031e2e2f28446ed968dd
Antonio <antonioserramitjana2005@gmail.com>
2025-05-05 09:21:54 +0200

    Clase de configuracion para poder urilizar las apis en la aplicacion en react

---
+++ src/main/java/com/AntonioP/YonkouTattoo/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.AntonioP.YonkouTattoo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+//Clase de configuracion creada para poder pasar la api a la url del frontend
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**") // o "/**" si prefieres todo
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
+
